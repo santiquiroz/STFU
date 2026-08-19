f775b234cfc954497b7b3a039a8e3a7e838075c1
Santiago Quiroz <santiqupgui@gmail.com>
2026-08-18 20:25:14 -0500

    test: cubrir teardown del pipeline en stop() del pipe server

--- backend/tests/test_apo_pipe_lifecycle.py
+++ backend/tests/test_apo_pipe_lifecycle.py
@@ -1,12 +1,45 @@
 import sys
 import time
+import numpy as np
 import pytest
+from stfu.core.audio_format import AudioFormat
 from stfu.core.pipeline import Pipeline
+from stfu.plugins.base import AudioPlugin, Parameter
 
 pytestmark = pytest.mark.skipif(sys.platform != "win32", reason="named pipes")
 
 _PIPE = r"\\.\pipe\stfu_test_lifecycle"
 _PIPE_HANDLERS = r"\\.\pipe\stfu_test_lifecycle_handlers"
+_PIPE_TEARDOWN = r"\\.\pipe\stfu_test_lifecycle_teardown"
+
+
+class _TeardownCountingPlugin(AudioPlugin):
+    name = "TeardownCounter"
+    version = "1.0.0"
+
+    def __init__(self) -> None:
+        self.teardown_calls = 0
+
+    @property
+    def preferred_format(self) -> AudioFormat:
+        return AudioFormat(48000, 2, 480)
+
+    def setup(self, fmt: AudioFormat) -> AudioFormat:
+        return fmt
+
+    def process(self, audio: np.ndarray) -> np.ndarray:
+        return audio
+
+    def teardown(self) -> None:
+        self.teardown_calls += 1
+
+    @property
+    def algorithmic_latency_ms(self) -> float:
+        return 0.0
+
+    @property
+    def parameters(self) -> list[Parameter]:
+        return []
 
 
 def test_stop_unblocks_accept_and_joins():
@@ -53,3 +86,17 @@ def test_stop_joins_handler_threads():
             assert server._client_threads == []
     finally:
         win32file.CloseHandle(client)
+
+
+def test_stop_tears_down_pipeline_plugins():
+    from stfu.apo.pipe_server import ApoPipeServer
+    plugin = _TeardownCountingPlugin()
+    pipeline = Pipeline()
+    pipeline.add_plugin(plugin)
+    server = ApoPipeServer(_PIPE_TEARDOWN, pipeline)
+    server.start()
+    time.sleep(0.2)  # el accept loop queda bloqueado en ConnectNamedPipe
+
+    server.stop()
+
+    assert plugin.teardown_calls == 1
